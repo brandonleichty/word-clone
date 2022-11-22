0ed638647fdda53b2447f7f62857d6f3cd3be9c9
Brandon Leichty <brandonl@me.com>
2022-11-22 09:12:05 -0800

    Add code changes for exercise one

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function GuessInput({ handlesGuess, maxLength, minLength }) {
+  const [guess, setGuess] = React.useState("");
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        handlesGuess(guess);
+        setGuess("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+        maxLength={maxLength}
+        minLength={minLength}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
